4fe527b1cf994997aa6a18fd846a0c0fbfee6c1a
Tomasz <stevest32@gmail.com>
2026-09-11 00:05:25 +0200

    apka b1c90f3894 (2026-09-11 00:05)

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    żeby HMI otwierało się od razu i bez zasięgu (dane z brokera i tak wymagają sieci -
    bez niej ekran pokazuje ostatni stan z napisem „czekam na pakiet”). Nowa wersja
    plików = nowa nazwa pamięci (WERSJA z odcisku treści) -> stare kopie znikają. */
-const WERSJA = 'basen-hmi-99669817f0';
+const WERSJA = 'basen-hmi-b1c90f3894';
 const PLIKI = ['./', './index.html', './hmi.html', './most_js.js', './paho-mqtt.min.js',
                './manifest.webmanifest', './ikona-192.png', './ikona-512.png', './ikona-maskable-512.png'];
 self.addEventListener('install', e => {
